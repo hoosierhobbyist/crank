dc53ec6d825b4dd6acf9f62a1d1a402e38581206
sedabull <sedabull@gmail.com>
2020-05-11 14:06:05 -0400

    update and refactor Emitter class

--- src/misc/emitter.js
+++ src/misc/emitter.js
@@ -1,102 +1,70 @@
-//emitter.js
-
 export default class Emitter {
-
-  constructor(_maxListeners = 10){
-    if(!Number.isInteger(_maxListeners)){
-      throw TypeError("maxListeners must be an integer");
-    }//end if
-    if(_maxListeners < 1){
-      throw RangeError("maxListeners must be at least 1");
-    }//end if
-
-    Object.defineProperties(this, {
-      events: {
-        enumerable: true,
-        value: new Map()
-      },//end events
-      maxListeners: {
-        enumerable: true,
-        get: function(){
-          return _maxListeners;
-        },//end get
-        set: function(value){
-          if(Number.isInteger(value)){
-            if(value > 0){
-              for(let [event, listeners] of this.events){
-                if(value < listeners.size){
-                  return void 0;
-                }//end if
-              }//end for
-
-              _maxListeners = value;
-            }//end if
-          }//end if
-        }//end set
-      }//end maxListeners
-    });//end defineProperties
-  }//end constructor
-
-  on(event, listener){
-    if(!(typeof listener === 'function')){
-      throw TypeError("listener must be a function");
-    }//end if
-
-    if(!this.events.has(event)){
-      this.events.set(event, new Set());
-    }//end if
-
-    if(this.events.get(event).size < this.maxListeners){
-      this.events.get(event).add(listener);
-    } else {
-      throw RangeError(`event ${event} cannot exceed ${this.maxListeners} listeners`);
-    }//end if/else
-
-    return this;
-  }//end on
-
-  once(event, listener){
-    if(!(typeof listener === 'function')){
-      throw TypeError("listener must be a function");
-    }//end if
-
-    if(!this.events.has(event)){
-      this.events.set(event, new Set());
-    }//end if
-
-    if(this.events.get(event).size < this.maxListeners){
-      let wrapper = function(self, args){
-        listener.apply(this, args);
-        this.remove(event, wrapper);
-      }//end wrapper
-      this.events.get(event).add(wrapper, this);
-    } else {
-      throw RangeError(`event ${event} cannot exceed ${this.maxListeners} listeners`);
-    }//end if/else
-
-    return this;
-  }//end once
-
-  emit(event, ...args){
-    if(this.events.has(event)){
-      this.events.get(event).forEach(function(listener) {
-        listener.apply(this, args);
-      }, this);//end forEach
-      return true;
-    }//end if
-
-    return false;
-  }//end emit
-
-  remove(event, listener){
-    if(this.events.has(event)){
-      if(this.events.get(event).has(listener)){
-        this.events.get(event).delete(listener);
-        return true;
-      }//end if
-    }//end if
-
-    return false;
-  }//end remove
-
+	
+	constructor() {
+		Object.defineProperties(this, {
+			_events: {
+				enumerable: true,
+				value: new Map()
+			},//end _events
+		});//end defineProperties
+	}//end constructor
+	
+	on(event, listener) {
+		if(!(typeof listener === 'function')){
+			throw TypeError("listener must be a function");
+		}//end if
+		
+		if(!this._events.has(event)){
+			this._events.set(event, new Set());
+		}//end if
+		this._events.get(event).add(listener);
+		
+		return this;
+	}//end on
+	
+	once(event, listener) {
+		if(!(typeof listener === 'function')){
+			throw TypeError("listener must be a function");
+		}//end if
+		
+		if(!this._events.has(event)){
+			this._events.set(event, new Set());
+		}//end if
+		
+		let wrapper = function(self, args){
+			listener.apply(this, args);
+			this.remove(event, wrapper);
+		}//end wrapper
+		this._events.get(event).add(wrapper, this);
+		
+		return this;
+	}//end once
+	
+	emit(event, ...args) {
+		if(this._events.has(event)){
+			this._events.get(event).forEach(function(listener) {
+				listener.apply(this, args);
+			}, this);//end forEach
+		}//end if
+		
+		return this;
+	}//end emit
+	
+	remove(event, listener) {
+		if(this._events.has(event)){
+			if(this._events.get(event).has(listener)){
+				this._events.get(event).delete(listener);
+				if(!this._events.get(event).size) {
+					this._events.delete(event);
+				}//end if
+			}//end if
+		}//end if
+		
+		return this;
+	}//end remove
+	
+	listening(event) {
+		return this._events.has(event);
+	}//end listening
+	
 }//end class Emitter
