f1a0588dedaa1e9c3d9abeb54b0087a7cd277bc7
sedabull <sedabull@gmail.com>
2020-05-21 15:23:23 -0400

    minor rework to Point class

--- src/geometry/point.js
+++ src/geometry/point.js
@@ -1,67 +1,57 @@
-//point.js
-
 class Point {
-
-  constructor(_x, _y, _ref = {x: 0, y: 0}){
-    if(typeof _ref.x !== 'number'){
-      throw TypeError("invalid x-value for reference object");
-    }//end if
-    if(typeof _ref.y !== 'number'){
-      throw TypeError("invalid y-value for reference object");
-    }//end if
-
-    Object.defineProperties(this, {
-      x: {
-        enumerable: true,
-        get: function(){
-          return _ref.x + _x;
-        },//end get
-        set: function(value){
-          value = parseFloat(value);
-          _x = value - _ref.x;
-        }//end set
-      },//end x
-      y: {
-        enumerable: true,
-        get: function(){
-          return _ref.y + _y;
-        },//end get
-        set: function(value){
-          value = parseFloat(value);
-          _y = value - _ref.y;
-        }//end set
-      },//end y
-      a: {
-        get: function(){
-          return Math.atan2(_y, _x);
-        },//end get
-        set: function(value){
-          let r = this.r;
-          _x = r * Math.cos(value);
-          _y = r * Math.sin(value);
-        }//end set
-      },//end angle
-      r: {
-        get: function(){
-          return Math.sqrt(sq(_x) + sq(_y));
-        },//end get
-        set: function(value){
-          let a = this.a;
-          _x = value * Math.cos(a);
-          _y = value * Math.sin(a);
-        }//end set
-      }//end radius
-    });//end defineProperties
-
-  }//end constructor
-
-  draw(ctx){
-    ctx.save();
-    ctx.fillStyle = "white";
-    ctx.beginPath();
-    ctx.arc(this.x, -this.y, 3 , 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.restore();
-  }//end draw
-
+    
+    constructor(x, y, ref = {x: 0, y: 0}) {
+        
+        Object.defineProperties(this, {
+            x: {
+                enumerable: true,
+                get: function() {
+                    return ref.x + x;
+                },//end get
+                set: function(value) {
+                    x = value - ref.x;
+                }//end set
+            },//end x
+            y: {
+                enumerable: true,
+                get: function() {
+                    return ref.y + y;
+                },//end get
+                set: function(value) {
+                    y = value - ref.y;
+                }//end set
+            },//end y
+            angle: {
+                get: function() {
+                    return Math.atan2(y, x);
+                },//end get
+                set: function(value) {
+                    let r = this.radius;
+                    x = r * Math.cos(value);
+                    y = r * Math.sin(value);
+                }//end set
+            },//end angle
+            radius: {
+                get: function() {
+                    return Math.sqrt(sq(x) + sq(y));
+                },//end get
+                set: function(value) {
+                    let a = this.angle;
+                    x = value * Math.cos(a);
+                    y = value * Math.sin(a);
+                }//end set
+            }//end radius
+        });//end defineProperties
+        
+    }//end constructor
+    
+    draw(ctx, radius = 3, color = 'white') {
+        ctx.save();
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(this.x, -this.y, radius , 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.restore();
+    }//end draw
+    
 }//end class Point
